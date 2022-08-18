62317762d1fd79b7f7f317171cbe783b7de36cc1
Danny Warren <dannydwarren@gmail.com>
2022-08-18 13:17:15 -0600

    Add calculation support for general calculation jobs, subtraction, multiplication, division, and combinations of those

--- Worker.Domain.UnitTests/CalculatorTests.cs
+++ Worker.Domain.UnitTests/CalculatorTests.cs
@@ -6,11 +6,21 @@ namespace Worker.Domain.UnitTests;
 public class CalculatorTests : UnitTestBase<Calculator>
 {
     [Theory]
+    [InlineData("CALCULATE: 0", 0)]
     [InlineData("0", 0)]
     [InlineData("1", 1)]
     [InlineData("1+1", 2)]
     [InlineData("2+2", 4)]
     [InlineData("2+2+2+2", 8)]
+    [InlineData("1*1", 1)]
+    [InlineData("2*3", 6)]
+    [InlineData("2+2*3", 8)]
+    [InlineData("2*3+2", 8)]
+    [InlineData("1-1", 0)]
+    [InlineData("-1+1", 0)]
+    [InlineData("2*-1", -2)]
+    [InlineData("2/2", 1)]
+    [InlineData("2/-1", -2)]
     public void When_calculating(string calculation, int expected)
     {
         var result = Because(() => ClassUnderTest.Calculate(calculation));
--- Worker.Domain/Calculator.cs
+++ Worker.Domain/Calculator.cs
@@ -9,7 +9,15 @@ public class Calculator : ICalculator
 {
     public int Calculate(string calculation)
     {
-        var parts = calculation.Split('+', StringSplitOptions.TrimEntries);
-        return parts.Select(int.Parse).Sum();
+        var noSpace = calculation.Replace(" ", "").Replace("CALCULATE:", "");
+        var convertedSubtraction = noSpace.Replace("-", "+-1*").TrimStart('+').Replace("*+", "*").Replace("/+", "/");
+        var additionParts = convertedSubtraction.Split('+', StringSplitOptions.TrimEntries);
+        var multiplicationParts = additionParts.Select(x => x.Split('*', StringSplitOptions.TrimEntries));
+        var divisionParts = multiplicationParts.Select(x => x.Select(y => y.Split('/', StringSplitOptions.TrimEntries)));
+        var result = divisionParts.Select(x => x.Select( y=>
+        {
+            return y.Select(int.Parse).Aggregate((a, b) => a / b);
+        }).Aggregate(1, (a, b) => a * b)).Sum();
+        return result;
     }
 }
